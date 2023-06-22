24b7e686483a3be3edbaa330738dbd617b0ebcc5
NeryHub <125278013+NeryHub@users.noreply.github.com>
2023-06-22 21:59:06 +0200

    Create mainContactForm.js
    Basic Simple and Functional contact form that uses local server to send email

---
+++ mainContactForm.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+
+function ContactForm() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    setIsLoading(true);
+    setIsSuccess(false);
+
+    const formData = {
+      name,
+      email,
+      message,
+    };
+
+    try {
+      const response = await fetch('http://localhost:5001/send-email', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        setIsSuccess(true);
+        setName('');
+        setEmail('');
+        setMessage('');
+      } else {
+        console.error('Failed to send email');
+      }
+    } catch (error) {
+      console.error('Error sending email:', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="message">Message:</label>
+        <textarea
+          id="message"
+          name="message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          required
+        ></textarea>
+      </div>
+      <button type="submit" disabled={isLoading}>
+        {isLoading ? 'Sending...' : isSuccess ? 'Success' : 'Submit'}
+      </button>
+    </form>
+  );
+}
+
+export default ContactForm;
